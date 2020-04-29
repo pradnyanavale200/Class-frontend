71a7c6054fc5f5c43fb39296e2925614e583904b
tejashreeSalvi <salvitejashree1999@gmail.com>
2020-04-29 22:31:44 +0530

    institute register page

--- src/app/institute-register/institute-register.component.ts
+++ src/app/institute-register/institute-register.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormsModule, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-institute-register',
@@ -7,9 +8,45 @@ import { Component, OnInit } from '@angular/core';
 })
 export class InstituteRegisterComponent implements OnInit {
 
+  name: string;
+  addrLine1: string;
+  addrLine2: string;
+  state = { id: -1, name: ''};
+  city = '';
+  pincode: number;
+
+  stateArray = [
+      { id: 0, state: 'Maharashtra'},
+      { id: 1, state: 'Tamil Nadu' },
+      { id: 2, state: 'Rajasthan'}
+  ];
+
+  cityArray = [
+    { id: 0, city: ['Pune', 'Mumbai', 'Nagpur', 'Thane', 'Pimpri'] },
+    { id: 1, city: ['Chennai', 'Coimbatore', 'Vellore', 'Tiruvannamalai'] },
+    { id: 2, city: ['Jaipur', 'Jodhpur' , 'Kota', 'Ajmer', 'Udaipur']}
+  ];
+
+
+  disabledcity = true;
+
+
   constructor() { }
 
-  ngOnInit(): void {
+  ngOnInit(): void { }
+
+  onchange() {
+    console.log("hello");
+    console.log(this.state.id);
+    if (this.state.id == -1) {
+      this.disabledcity = true;
+    }
+    else {
+      this.disabledcity = false;
+    }
+  }
+  onSubmit(value){
+    console.log(value);
   }
 
 }
