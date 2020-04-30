52557266fc55abf7216839e0aa64fb68e5f98a68
Vini Mehta <vini.mehta78@gmail.com>
2020-05-01 00:05:38 +0530

    course update UI and functionality done(Services remaining)

--- src/app/dashboard/course/course-update/course-update.component.ts
+++ src/app/dashboard/course/course-update/course-update.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, FormBuilder, Validators } from '@angular/forms';
+import { Router, ActivatedRoute } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
+
+import { CourseService } from '../services/course.service';
 
 @Component({
   selector: 'app-course-update',
@@ -7,9 +12,78 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CourseUpdateComponent implements OnInit {
 
-  constructor() { }
+  courseUpdateForm: FormGroup;
+  public courseId = ' ';
+  public course = {
+    courseName: '',
+    duration: '',
+    value: '',
+    fees: ''
+  };
+  constructor(
+    private fb: FormBuilder,
+    private route: ActivatedRoute,
+    private router: Router,
+    private http: HttpClient
+
+  ) { }
 
   ngOnInit(): void {
+    const id = this.route.snapshot.paramMap.get('id');
+    this.courseId = id;
+
+    const data = {id : this.courseId};
+
+    this.http.post('http://localhost:3000/dashboard/course/data', data).subscribe((response: any) => {
+      this.course = response.course;
+      this.setData(this.course);
+    }, (error) => {
+      console.log(error);
+    });
+    this.courseUpdateForm = this.fb.group({
+      courseName: ['', [Validators.required, Validators.pattern('[a-zA-Z0-9._,-/]{3,50}')]],
+      duration: ['', [Validators.required, Validators.pattern('[0-9]{1,2}')]],
+      value: ['', Validators.required],
+      fees: ['', [Validators.required, Validators.pattern('[0-9]')]]
+    });
   }
 
+  setData(course) {
+    this.courseUpdateForm.get('courseName').setValue(this.course.courseName);
+    this.courseUpdateForm.get('duration').setValue(this.course.duration);
+    this.courseUpdateForm.get('value').setValue(this.course.value);
+    this.courseUpdateForm.get('fees').setValue(this.course.fees);
+  }
+
+  get courseNameValidate() {
+    return this.courseUpdateForm.get('courseName');
+  }
+
+  get durationValidate() {
+    return this.courseUpdateForm.get('duration');
+  }
+
+  get valueValidate() {
+    return this.courseUpdateForm.get('value');
+  }
+
+  get feesValidate() {
+    return this.courseUpdateForm.get('fees');
+  }
+
+  updateCourse(){
+    const data = {
+      id : this.courseId,
+      courseName : this.courseUpdateForm.get('courseName').value,
+      duration : this.courseUpdateForm.get('duration').value,
+      value : this.courseUpdateForm.get('value').value,
+      fees : this.courseUpdateForm.get('fees').value
+    };
+
+    this.http.post('http://localhost:3000/dashboard/course/update', data).subscribe((response: any) => {
+      this.router.navigate(['../../list']);
+    }, (error) => {
+      console.log(error);
+    });
+  }
 }
--- src/app/dashboard/course/services/course.service.ts
+++ src/app/dashboard/course/services/course.service.ts
@@ -1,9 +1,21 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CourseService {
 
-  constructor() { }
+  courseApi = 'http://localhost:3000/dashboard/course';
+  constructor(
+    private http: HttpClient
+  ) {   }
+
+  courseUpdate(data){
+    return this.http.get(this.courseApi + '/courseUpdate', data);
+  }
+
+  getCourseData(data){
+    return this.http.post(this.courseApi + '/courseData', data);
+  }
 }
