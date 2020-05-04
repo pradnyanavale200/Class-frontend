c142698b494c42a6d1871f5ab1833a3adbe59cdc
Deep Patadiya <36883468+MrDx24@users.noreply.github.com>
2020-05-04 17:42:54 +0530

    Updated student update component

--- src/app/dashboard/student/student-update/student-update.component.ts
+++ src/app/dashboard/student/student-update/student-update.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { StudentService } from '../services/student.service';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 import { ActivatedRoute, Router } from '@angular/router';
+import { IDropdownSettings } from 'ng-multiselect-dropdown';
 
 @Component({
   selector: 'app-student-update',
@@ -13,6 +14,14 @@ export class StudentUpdateComponent implements OnInit {
   list = [ {no: '1'}, {no: '2'}, {no: '3'}, {no: '4'}, {no: '5'}, ];
 
   studentupdateform: FormGroup;
+
+  Courses: any = [];
+  disabled = false;
+  ShowFilter = false;
+  limitSelection = false;
+  selectedItems: any = [];
+  dropdownSettings: IDropdownSettings = {};
+
   public studId = ' ';
   public student = {
     firstname: '',
@@ -31,24 +40,42 @@ export class StudentUpdateComponent implements OnInit {
   ngOnInit() {
     const id = this.route.snapshot.paramMap.get('id');
     this.studId = id;
-
+    console.log(this.studId);
     const data = {id : this.studId};
 
-    this.studentService.getStudent(data).subscribe((response: any) => {
+    this.studentService.getStudent(this.studId).subscribe((response: any) => {
         this.student = response.student;
-        this.setData(this.student);
-        console.log(this.student);
+        // this.Courses = response.student.
+        this.setData(response.student);
+        console.log('response : ', response.student);
       }, (error) => {
         console.log(error);
-        alert(error.error.message);
+        // alert(error.error.message);
     });
 
     this.studentupdateform = this.fb.group({
       firstname: ['', Validators.required],
       lastname: ['', Validators.required],
       email: ['', Validators.required],
-      courses: [[], Validators.required],
+      courses: [this.selectedItems, Validators.required],
     });
+
+    this.Courses = [
+      { item_id: 1, item_text: 'CPP' },
+      { item_id: 2, item_text: 'C' },
+      { item_id: 3, item_text: 'JAVA' },
+      { item_id: 4, item_text: 'PYTHON' },
+    ];
+
+    this.dropdownSettings = {
+      singleSelection: false,
+      idField: 'item_id',
+      textField: 'item_text',
+      selectAllText: 'Select All',
+      unSelectAllText: 'UnSelect All',
+      itemsShowLimit: 5,
+      allowSearchFilter: true,
+    };
   }
 
   setData(student) {
@@ -82,10 +109,11 @@ export class StudentUpdateComponent implements OnInit {
       email: this.studentupdateform.get('email').value,
       courses: this.studentupdateform.get('courses').value
     };
+    console.log(data);
 
     this.studentService.updateStudent(data).subscribe((response: any) => {
         console.log(response);
-        alert(response.msg);
+        // alert(response.msg);
         this.router.navigate(['/dashboard/student/list']);
       }, (error) => {
         console.log(error);
