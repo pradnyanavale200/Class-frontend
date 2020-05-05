4a147e81706a75710d05c3cc73425b0a4b6d3cc0
pradnya-navale <navalepk17.it@coep.ac.in>
2020-05-04 21:14:22 -0700

    institute service

--- src/app/dashboard/institute/services/institute.service.ts
+++ src/app/dashboard/institute/services/institute.service.ts
@@ -1,9 +1,32 @@
 import { Injectable } from '@angular/core';
+import { HttpService } from 'src/app/core/services/http.service';
+import { environment } from 'src/environments/environment';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class InstituteService {
+  instituteApi = `${environment.api}/institute`;
 
-  constructor() { }
+  constructor(private httpService: HttpService) { }
+
+  getInstitutes(ownerId): Observable<object> {
+    return this.httpService.get(`${this.instituteApi}/${ownerId}`);
+  }
+
+  createInstitute(institute): Observable<object> {
+    return this.httpService.post(
+      `${this.instituteApi}/register`,
+      institute
+    );
+  }
+
+  deleteInstitute(instituteId): Observable<object> {
+    return this.httpService.delete(`${this.instituteApi}/${instituteId}`);
+  }
+
+  updateInstitute(data): Observable<object> {
+    return this.httpService.put(`${this.instituteApi}/${data.instId}`, data);
+  }
 }
