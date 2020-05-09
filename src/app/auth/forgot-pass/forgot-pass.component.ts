5aaa297a34414e6b1b09e5dbf6eff4769f3aea13
Shubham-Gawade <shubham.gawade7292@gmail.com>
2020-05-09 18:52:36 +0530

    Send Email done

--- src/app/auth/forgot-pass/forgot-pass.component.ts
+++ src/app/auth/forgot-pass/forgot-pass.component.ts
@@ -7,10 +7,9 @@ import { Router } from '@angular/router';
 @Component({
   selector: 'app-forgot-pass',
   templateUrl: './forgot-pass.component.html',
-  styleUrls: ['./forgot-pass.component.css']
+  styleUrls: ['./forgot-pass.component.css'],
 })
 export class ForgotPassComponent implements OnInit {
-
   forgotPasswordForm: FormGroup;
   emailExample: '< example > @ < mail >.< com >';
   togglePassword = 'visibility';
@@ -21,46 +20,31 @@ export class ForgotPassComponent implements OnInit {
     private fb: FormBuilder,
     private auth: AuthService,
     private router: Router
-  ) { }
+  ) {}
 
   ngOnInit(): void {
     this.forgotPasswordForm = this.fb.group({
       email: ['', [Validators.required, Validators.pattern(REGEX.EMAIL)]],
-      newPassword: ['', [Validators.required, Validators.pattern(REGEX.PASSWORD)]],
     });
   }
 
   get email() {
     return this.forgotPasswordForm.get('email');
   }
-  get newPassword() {
-    return this.forgotPasswordForm.get('newPassword');
-  }
 
-  toggle() {
-    if (this.disabledPassword == true) {
-      this.disabledPassword = false;
-      this.togglePassword = 'visibility_off';
-      this.typePassword = 'text';
-    } else {
-      this.disabledPassword = true;
-      this.togglePassword = 'visibility';
-      this.typePassword = 'password';
-    }
-  }
-
-  onClick(){
+  onClick() {
     const data = {
       email: this.forgotPasswordForm.get('email').value,
-      password: this.forgotPasswordForm.get('newPassword').value,
     };
-    this.auth.forgotPassword(data).subscribe((res: any) => {
-      this.router.navigate(['./auth/login']);
-    }, (err: any) => {
-      alert('Error in login');
-    });
 
+    this.auth.forgotPassword(data).subscribe(
+      (res: any) => {
+        alert(res.message);
+        this.router.navigate(['./auth/login']);
+      },
+      (err: any) => {
+        alert('Error in Forget Password');
+      }
+    );
   }
-
-
 }
