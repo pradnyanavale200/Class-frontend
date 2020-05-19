50f565df6458a4b4468693eeec0abf4877cde65e
Shubham-Gawade <shubham.gawade7292@gmail.com>
2020-05-19 14:38:21 +0530

    Deployment Updated

--- server.js
+++ server.js
@@ -4,10 +4,8 @@ const path=require('path');
 
 const app=express();
 
-app.use(express.static(__dirname)+'/angularapp');
-
 app.get('/*',function(req,res) {
-  res.sendFile(path.join(__dirname+'angularapp/index.html'));
+  res.sendFile(path.join(__dirname+'/angularapp/index.html'));
 });
 
-app.listen(process.env.PORT || 8080);
+app.listen(process.env.PORT || 4200);
